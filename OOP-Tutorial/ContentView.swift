f1e67f99d29e79774e0d1d87914ef9ca6d966fc0
Ryan Keys <r.keys1998@gmail.com>
2020-11-20 22:59:42 -0800

    Initial Commit

---
+++ OOP-Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OOP-Tutorial
+//
+//  Created by Ryan Keys on 11/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OOP-Tutorial/OOP_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  OOP_TutorialApp.swift
+//  OOP-Tutorial
+//
+//  Created by Ryan Keys on 11/20/20.
+//
+
+import SwiftUI
+
+@main
+struct OOP_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
